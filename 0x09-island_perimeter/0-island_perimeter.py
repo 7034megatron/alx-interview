f7f8207a70f55e8a341cde55630cc67fc3844060
7034megatron <emmanuelmabhena0@gmail.com>
2024-08-29 19:01:28 +0000

    updated code

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,27 +1,34 @@
- #!/usr/bin/python3
-"""Island Perimeter Problem
+#!/usr/bin/python3
+"""
+Module for island perimeter.
 """
 
 
 def island_perimeter(grid):
     """
-    Calculates the perimeter of the island described in grid
-    Args:
-        grid: 2d list of integers containing 0(water) or 1(land)
-    Return:
-        the perimeter of the island
-    """
+    Calculates the perimeter of the island in the grid.
 
-    p = 0
+    Args:
+        grid (List[List[int]]): A list of lists representing the island.
+
+    Returns:
+        int: The perimeter of the island.
+
+    Raises:
+        ValueError: If the grid is not rectangular.
+    """
+    if not all(len(row) == len(grid[0]) for row in grid):
+        raise ValueError("Grid is not rectangular")
+
+    perimeter = 0
+
     for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if (grid[i][j] == 1):
-                if (i <= 0 or grid[i - 1][j] == 0):
-                    p += 1
-                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
-                    p += 1
-                if (j <= 0 or grid[i][j - 1] == 0):
-                    p += 1
-                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
-                    p += 1
-    return p
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
